a7e4177b17455ea63e24fe04395229777d1b57a3
TheAbsconder <mtbachand@gmail.com>
2025-03-24 15:46:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Wedding Info</title>
+</head>
+<body>
+  <!--<h1>Welcome to Our Wedding Info Page</h1>-->
+  <p style="font-size: xx-large;">Test, It Worked!</p>
+</body>
+</html>
